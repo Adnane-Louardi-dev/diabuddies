5cd770e86a94624b4c31514b2a8e772cce07c733
Adnane Louardi <adnanelouardi.dev@gmail.com>
2022-12-01 01:31:16 +0100

    add MUI package

--- client/pages/_document.js
+++ client/pages/_document.js
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter&family=Poppins&family=Roboto&family=Rubik&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:300,400,500,700&family=Inter&family=Poppins&family=Rubik&display=swap" rel="stylesheet" />
       </Head>
       <body>
         <Main />
