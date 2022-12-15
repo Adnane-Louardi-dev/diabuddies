6292558ff386ea9a9f49fee73812563a0ffee674
Adnane Louardi <adnanelouardi.dev@gmail.com>
2022-12-15 05:14:59 +0100

    enhace login page

--- client/pages/login.js
+++ client/pages/login.js
@@ -10,11 +10,15 @@ export default function Login() {
   const onSubmit = (data) => setFormData(JSON.stringify(data));
   return (
     <div className="h-full h-screen">
+      <div className="absolute z-50 m-5">
+        <div className="text-inter text-diaBlue text-3xl font-semibold">
+          DIA<span className="text-diaOrange">BUDDIES</span>
+        </div>
+      </div>
       <div className="flex flex-row items-center h-screen ">
-        <div className="flex-none justify-center content-center basis-1/3 px-20">
+        <div className="flex-none justify-center content-center basis-2/5 px-20">
           <div className="my-5 py-5">
-            <h1 className="mb-2 text-3xl font-inter font-semibold">Create an Account</h1>
-            <span className="text-md text-gray-400 font-inter">Let's manage your diabetes together.</span>
+            <h1 className="mb-2 text-3xl font-inter font-semibold">Login to your panel</h1>
             <div className="py-5 border-b">
               <Link href="http://localhost:3000/login/auth/google" passHref>
                 <Button fullWidth variant="outlined" className="mt-7 text-diaGray border-diaGray" startIcon={<GoogleIcon />}>
@@ -50,15 +54,16 @@ export default function Login() {
             </form>
           </div>
         </div>
-        <div className="flex-1 rounded-lg bg-diaGreen w-full h-full basis-1/6">
-          <div className="m-5">
-            <div className="text-inter text-diaBlue text-2xl font-semibold">
-              DIA<span className="text-diaOrange">BUDDIES</span>
-            </div>
+        <div className="relative flex-1 rounded-lg shadow-lg w-full h-full basis-1/5">
+          <div className="absolute m-16 z-50">
+            <h2 className="mb-8 font-rubik text-4xl text-white">
+              Let's put some control <br /> on your diabetes together.
+            </h2>
+            <span className="text-lg text-white font-inter">
+              With Diabuddies you can easily control <br /> your chronic disease and your Blood Sugar.
+            </span>
           </div>
-          <div className="">
-            <Image src={"/blackWomanChecking.png"} height={300} width={500} alt={"black woman checking blood sugar"} />
-          </div>
+          <Image src={"/signup_promo.jpg"} className="blur-xs brightness-50" fill objectFit="cover" alt={"black woman checking blood sugar"} />
         </div>
       </div>
     </div>
