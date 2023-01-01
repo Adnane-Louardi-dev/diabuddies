7eb72accd7da8a05e29180726a5192a9857d0627
Adnane Louardi <adnanelouardi.dev@gmail.com>
2023-01-01 22:43:56 +0100

    use the new logo

--- client/pages/login.js
+++ client/pages/login.js
@@ -12,36 +12,34 @@ export default function Login() {
   const onSubmit = (data) => setFormData(JSON.stringify(data));
   return (
     <div className="h-full h-screen">
-      <div className="relative lg:hidden h-96 w-screen pt-16 px-8">
-        {/* <h1 className="text-6xl text-white font-rubik font-semibold tracking-wider">Login to your panel</h1> */}
-        <p className="text-4xl text-white tracking-wide leading-relaxed font-poppins">
-          With <span className="font-semibold hover:underline hover:text-diaBlue decoration-diaOrange">DIABUDDIES</span> you can easily control <br />
+      <div className="relative lg:hidden h-52 w-screen pt-8 px-6">
+        <p className="text-md text-white leading-relaxed font-poppins">
+          With <span className="font hover:underline hover:text-diaBlue decoration-diaOrange">DIABUDDIES</span> you can easily control <br />
           and track your diabetes in one single platform.
-          <Button endIcon={<EastIcon />} className="my-6 font-roboto text-white hover:text-diaOrange text-4xl underline hover:decoration-diaOrange">
+          <br />
+          <Button endIcon={<EastIcon />} className="font-roboto text-white hover:text-diaOrange text-md underline hover:decoration-diaOrange">
             Discover why and how
           </Button>
         </p>
 
-        <Image src={"/signup_promo.jpg"} className="brightness-50 -z-50" fill objectFit="cover" alt={"black woman checking blood sugar"} />
+        <Image src={"/signup_promo.jpg"} className="brightness-50 blur-xs -z-50" fill objectFit="cover" alt={"black woman checking blood sugar"} />
       </div>
-      <div className="flex flex-row -mt-12 bg-white justify-center lg:items-center h-full w-full rounded-t-[25px] lg:rounded-none">
-        <div className="absolute my-16">
-          <div className="text-inter text-diaBlue text-7xl font-semibold">
-            DIA<span className="text-diaOrange">BUDDIES</span>
-          </div>
+      <div className="flex flex-row -mt-12 bg-white justify-center lg:items-center h-full w-full rounded-3xl lg:rounded-none">
+        <div className="absolute">
+          <Image src={"/diabuddies_logo.jpg"} height={100} width={250} />
         </div>
-        <div className="flex-none w-full lg:basis-2/5 my-48">
+        <div className="flex-none w-full lg:basis-2/5 my-28">
           <div className="my-5 p-10">
-            <div className="mb-12 pb-10 border-b-4">
+            <div className="mb-5 pb-5 border-b-2">
               <Link href="/api/login/auth/google" passHref>
-                <Button fullWidth variant="outlined" className="mb-7 p-7 text-diaGray text-4xl rounded-full border-4 border-diaGray">
-                  <GoogleIcon className="text-5xl mx-5" />
+                <Button fullWidth variant="outlined" className="mb-7 p-2 text-diaGray text-md rounded-full border-2 border-diaGray">
+                  <GoogleIcon className="text-md mx-5" />
                   Sign up with Google
                 </Button>
               </Link>
               <Link href="/api/login/auth/facebook" to="http://localhost:3000/login/auth/facebook" as="/login" passHref>
-                <Button fullWidth variant="outlined" className="mb-5 p-7 text-diaGray text-4xl rounded-full border-4 border-diaGray">
-                  <FacebookIcon className="text-5xl mx-5" />
+                <Button fullWidth variant="outlined" className="mb-7 p-2 text-diaGray text-md rounded-full border-2 border-diaGray">
+                  <FacebookIcon className="text-md mx-5" />
                   Sign up with Facebook
                 </Button>
               </Link>
@@ -52,16 +50,15 @@ export default function Login() {
                 required
                 InputLabelProps={{
                   style: {
-                    fontSize: 45,
+                    fontSize: 16,
                     backgroundColor: "transparent",
                     color: "#666666",
                   },
                 }}
                 inputProps={{
                   style: {
-                    fontSize: 45,
+                    fontSize: 16,
                     backgroundColor: "transparent",
-                    margin: "25px 0",
                   },
                 }}
                 {...register("email", { pattern: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ })}
@@ -75,16 +72,15 @@ export default function Login() {
                 required
                 InputLabelProps={{
                   style: {
-                    fontSize: 45,
+                    fontSize: 16,
                     backgroundColor: "transparent",
                     color: "#666666",
                   },
                 }}
                 inputProps={{
                   style: {
-                    fontSize: 45,
+                    fontSize: 16,
                     backgroundColor: "transparent",
-                    margin: "25px 0",
                   },
                 }}
                 {...register("password")}
@@ -98,7 +94,7 @@ export default function Login() {
               />
               <Button
                 fullWidth
-                className="my-10 p-7 text-diaGray text-4xl rounded-full border-4 border-diaGray"
+                className="my-10 p-2 text-diaGray text-md rounded-full border-2 border-diaGray"
                 variant="outlined"
                 type="submit"
                 margin="normal"
